c90c2da4c3d0ba8b518c638efadedefa3dc394ab
nkmbxy <nakamon22547@gmail.com>
2024-12-03 16:10:34 +0700

    update disease reccomment

--- dist/controllers/logger/addLogger.js
+++ dist/controllers/logger/addLogger.js
@@ -22,10 +22,28 @@ const addLogger = (req, res) => __awaiter(void 0, void 0, void 0, function* () {
         // if (!file) {
         //   return res.status(400).send("Please upload a file");
         // }
-        const imageUrl = yield (0, uploadImage_1.uploadImage)(file);
-        const predictImage = yield axios_1.default.post("http://127.0.0.1:8000/predict", { imageArr: [imageUrl] }).then((response) => {
+        const imageUrl = yield (0, uploadImage_1.uploadImage)(req.file);
+        const predictImage = yield axios_1.default
+            .post("http://127.0.0.1:8000/predict", { imageArr: [imageUrl] })
+            .then((response) => {
             return response.data;
         }).catch((error) => { console.log(error); });
+        if (!predictImage) {
+            res.status(400).json({ message: "Prediction service returned no data." });
+            return;
+        }
+        const predictedDiseaseName = predictImage.prediction;
+        if (!predictedDiseaseName) {
+            res.status(400).json({ message: "No detectable disease found." });
+            return;
+        }
+        const disease = yield model_1.DiseaseModel.findOne({ DiseaseName: predictedDiseaseName });
+        if (!disease) {
+            res.status(404).json({
+                message: "Disease not found in database.",
+            });
+            return;
+        }
         const { temperature, air_humidity, soil_moisture, nitrogen, phosphorus, potassium } = req.body;
         const temperatureData = new model_1.TemperatureModel({ value: temperature });
         const airHumidityData = new model_1.AirHumidityModel({ value: air_humidity });
@@ -33,31 +51,39 @@ const addLogger = (req, res) => __awaiter(void 0, void 0, void 0, function* () {
         const nitrogenData = new model_1.NitrogenModel({ value: nitrogen });
         const phosphorusData = new model_1.PhosphorusModel({ value: phosphorus });
         const potassiumData = new model_1.PotassiumModel({ value: potassium });
-        const predictImageName = new model_1.DiseasePredictModel({ image: imageUrl });
+        const diseasePredict = new model_1.DiseasePredictModel({
+            image: imageUrl,
+            disease_id: disease._id,
+        });
         yield temperatureData.save();
         yield airHumidityData.save();
         yield soilMoistureData.save();
         yield nitrogenData.save();
         yield phosphorusData.save();
         yield potassiumData.save();
-        yield predictImageName.save();
-        const Logger = new model_1.LoggerModel({
+        yield diseasePredict.save();
+        const logger = new model_1.LoggerModel({
             temperature_id: temperatureData._id,
             air_humidity_id: airHumidityData._id,
             soil_moisture_id: soilMoistureData._id,
             nitrogen_id: nitrogenData._id,
             phosphorus_id: phosphorusData._id,
             potassium_id: potassiumData._id,
-            diseasePredict_id: predictImageName._id,
+            diseasePredict_id: diseasePredict._id,
             date: new Date(),
         });
-        yield Logger.save();
-        res.send({
-            message: "Logger added successfully",
+        yield logger.save();
+        res.status(200).json({
+            message: "Logger saved successfully",
+            diseasePrediction: {
+                disease_id: disease._id,
+                DiseaseName: disease.DiseaseName,
+                Recommend: disease.Recommend,
+            },
         });
     }
     catch (error) {
-        console.log(error);
+        console.log(error.message);
     }
 });
 exports.addLogger = addLogger;
--- src/controllers/logger/addLogger.ts
+++ src/controllers/logger/addLogger.ts
@@ -19,40 +19,73 @@ export const addLogger = async (req: Request, res: Response) => {
     // if (!file) {
     //   return res.status(400).send("Please upload a file");
     // }
-    const imageUrl = await uploadImage(file);
-    const predictImage = await axios.post("http://127.0.0.1:8000/predict",{imageArr:[imageUrl]}).then((response) => {
-      return response.data;
-    }).catch((error) => {console.log(error)});
-    const {temperature, air_humidity, soil_moisture, nitrogen, phosphorus, potassium} = req.body;
-    const temperatureData = new TemperatureModel({value:temperature});
-    const airHumidityData = new AirHumidityModel({value:air_humidity});
-    const soilMoistureData = new SoilMoistureModel({value:soil_moisture});
-    const nitrogenData = new NitrogenModel({value:nitrogen});
-    const phosphorusData = new PhosphorusModel({value:phosphorus});
-    const potassiumData = new PotassiumModel({value:potassium});
-    const predictImageName = new DiseasePredictModel({image:imageUrl});
+    const imageUrl = await uploadImage(req.file);
+
+    const predictImage = await axios
+      .post("http://127.0.0.1:8000/predict", { imageArr: [imageUrl] })
+      .then((response) => {
+        return response.data;
+      }).catch((error) => {console.log(error)});
+    
+      if (!predictImage) {
+        res.status(400).json({ message: "Prediction service returned no data." });
+        return;
+      }
+
+    const predictedDiseaseName = predictImage.prediction;
+    if (!predictedDiseaseName) {
+      res.status(400).json({ message: "No detectable disease found." });
+      return;
+    }
+    
+    const disease = await DiseaseModel.findOne({ DiseaseName: predictedDiseaseName });
+    if (!disease) {
+      res.status(404).json({
+        message: "Disease not found in database.",
+      });
+      return;
+    }
+    
+    const { temperature, air_humidity, soil_moisture, nitrogen, phosphorus, potassium } = req.body;
+    const temperatureData = new TemperatureModel({ value: temperature });
+    const airHumidityData = new AirHumidityModel({ value: air_humidity });
+    const soilMoistureData = new SoilMoistureModel({ value: soil_moisture });
+    const nitrogenData = new NitrogenModel({ value: nitrogen });
+    const phosphorusData = new PhosphorusModel({ value: phosphorus });
+    const potassiumData = new PotassiumModel({ value: potassium });
+    const diseasePredict = new DiseasePredictModel({
+      image: imageUrl,
+      disease_id: disease._id,
+    });
     await temperatureData.save();
     await airHumidityData.save();
     await soilMoistureData.save();
     await nitrogenData.save();
     await phosphorusData.save();
     await potassiumData.save();
-    await predictImageName.save();
-  const Logger = new LoggerModel({
-    temperature_id: temperatureData._id,
-    air_humidity_id: airHumidityData._id,
-    soil_moisture_id: soilMoistureData._id,
-    nitrogen_id: nitrogenData._id,
-    phosphorus_id: phosphorusData._id,
-    potassium_id: potassiumData._id,
-    diseasePredict_id: predictImageName._id,
-    date: new Date(),
-  })
-    await Logger.save();
-    res.send({
-      message: "Logger added successfully",
+    await diseasePredict.save();
+
+    const logger = new LoggerModel({
+      temperature_id: temperatureData._id,
+      air_humidity_id: airHumidityData._id,
+      soil_moisture_id: soilMoistureData._id,
+      nitrogen_id: nitrogenData._id,
+      phosphorus_id: phosphorusData._id,
+      potassium_id: potassiumData._id,
+      diseasePredict_id: diseasePredict._id,
+      date: new Date(),
+    });
+    await logger.save();
+
+    res.status(200).json({
+      message: "Logger saved successfully",
+      diseasePrediction: {
+        disease_id: disease._id,
+        DiseaseName: disease.DiseaseName,
+        Recommend: disease.Recommend,
+      },
     });
   } catch (error: any) {
-    console.log(error);
+    console.log(error.message);
   }
 };
